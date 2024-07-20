15ba2d7e6248d02085afdfd7f9556ee5f48d4af3
tanyppp <947860982@qq.com>
2024-07-20 22:41:32 +0800

    Move run function to bottom for really run

--- js/prototype-chain.js
+++ js/prototype-chain.js
@@ -1,10 +1,6 @@
 (
   function (w, d) {
 
-    run('objectCreate', objectCreate, {});
-    run('objectCreate', objectCreate, []);
-    // run('objectCreate', objectCreate);
-
     function objectCreate(target) {
       if (typeof target !== 'object') throw new Error('The first parameter is not of object type');
 
@@ -14,5 +10,8 @@
       return obj;
     }
 
+    run('objectCreate', objectCreate, {});
+    run('objectCreate', objectCreate, []);
+    // run('objectCreate', objectCreate);
   }
 )(window, document);
