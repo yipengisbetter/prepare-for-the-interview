3f2374e4e851ec9a1107fbde14d9574cd3b1a02c
yipengisbetter <yipengtancanbebetter@gmail.com>
2024-07-21 09:16:17 +0800

    First finish bind / call / apply's logic

--- js/bind-call-apply.js
+++ js/bind-call-apply.js
@@ -1,5 +1,40 @@
 (
   function (w, d) {
+    function myBind(fn, b) {
+      if (typeof fn !== "function" && typeof b !== "object") return;
 
+      return function () {
+        return fn.apply(b, arguments);
+      }
+    }
+
+    function myCall(fn, b) {
+      if (typeof fn !== "function" && typeof b !== "object") return;
+
+      b["@@fn"] = fn;
+
+      var result = b["@@fn"]();
+
+      delete b["@@fn"];
+
+      return result;
+    }
+
+    function myApply(fn, b, param) {
+      if (typeof fn !== "function" && typeof b !== "object") return;
+
+      b["@@fn"] = fn;
+
+      var result = b["@@fn"](...param);
+
+      delete b["@@fn"];
+
+      return result;
+    }
+
+    var bindResult = run("myBind", myBind, function () { return this; }, { type: "test" });
+    console.log(bindResult());
+    run("myCall", myCall, function () { return this; }, { type: "test" });
+    run("myApply", myApply, function (name) { return { this: this, name }; }, { type: "test" }, ["apply test"]);
   }
 )(window, document);
